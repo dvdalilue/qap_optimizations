5ae884f5fae2a1a97bfb6a6b560da77721776a7d
David Lilue <dvdalilue@gmail.com>
2018-06-02 15:38:00 +0200

    Eager local search with incremental mutation

--- local_search.py
+++ local_search.py
@@ -1,6 +1,36 @@
+import math
 import random
 
-LOCAL_SEARCH_COEFFICIENT = 1.27
+LOCAL_SEARCH_COEFFICIENT = 5.27
+
+def eager_search(sln, iterations_coeff=LOCAL_SEARCH_COEFFICIENT):
+    options = xrange(0, sln.n)
+    a_random = random.SystemRandom()
+
+    current_ite = 0
+    max_ite_f = iterations_coeff * sln.n
+    max_ite_i = int(max_ite_f)
+
+    while current_ite < max_ite_i:
+        current_ite += 1
+        mutation_factor = float(current_ite) / max_ite_f
+
+        aux_sln = sln.copy()
+
+        for _ in xrange(0, int(math.ceil(mutation_factor * sln.n))):
+            city_1 = a_random.choice(options)
+            city_2 = a_random.choice(options)
+
+            aux_sln.exchangeFacilities(city_1, city_2)
+
+            if aux_sln.cost < sln.cost:
+                sln.permutation = aux_sln.permutation
+                sln.flows = aux_sln.flows
+                sln.cost = aux_sln.cost
+                current_ite = 0
+                break
+
+        del aux_sln
 
 def search(sln, iterations_coeff=LOCAL_SEARCH_COEFFICIENT):
     options = xrange(0,sln.n)
