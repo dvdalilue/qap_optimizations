e9c7c4d9b5578396ecf37e9524f98bae3d96c35a
David Lilue <dvdalilue@gmail.com>
2018-06-02 15:37:05 +0200

    Equivalence ratio method between arrays/lists

--- np_functions.py
+++ np_functions.py
@@ -9,3 +9,12 @@ def swapColumns(matrix, j_1, j_2):
     matrix = matrix.transpose()
     swapRows(matrix, j_1, j_2)
     matrix = matrix.transpose()
+
+def equivalenceRatio(a_1, a_2):
+    n = len(a_1) # == len(a_2)
+    equivalence = 0.0
+    for i in xrange(0, n):
+        if a_1[i] == a_2[i]:
+            equivalence += 1.0
+
+    return (equivalence / float(n))
