50b9a84eb20fadc9c998a1fdc9b539a864f249ce
David Lilue <dvdalilue@gmail.com>
2018-06-03 04:19:19 +0200

    Simulated annealing implementation. Code refactoring

--- local_search.py
+++ local_search.py
@@ -1,48 +1,66 @@
 import math
 import random
 
+from helper_functions import randomOptions
+
 LOCAL_SEARCH_COEFFICIENT = 5.27
+ANNEALING_TEMPERATURE_FACTOR = 1000
 
 def eager_search(sln, iterations_coeff=LOCAL_SEARCH_COEFFICIENT):
-    options = xrange(0, sln.n)
-    a_random = random.SystemRandom()
+    crnt_it = 0.0
+    max_it_f = iterations_coeff * sln.n
 
-    current_ite = 0
-    max_ite_f = iterations_coeff * sln.n
-    max_ite_i = int(max_ite_f)
-
-    while current_ite < max_ite_i:
-        current_ite += 1
-        mutation_factor = float(current_ite) / max_ite_f
-
+    while crnt_it < max_it_f:
+        crnt_it += 1.0
+        mutation_factor = crnt_it / max_it_f
         aux_sln = sln.copy()
 
-        for _ in xrange(0, int(math.ceil(mutation_factor * sln.n))):
-            city_1 = a_random.choice(options)
-            city_2 = a_random.choice(options)
-
-            aux_sln.exchangeFacilities(city_1, city_2)
+        for i in xrange(0, int(math.ceil(mutation_factor * sln.n))):
+            aux_sln.randomize()
 
             if aux_sln.cost < sln.cost:
                 sln.permutation = aux_sln.permutation
                 sln.flows = aux_sln.flows
                 sln.cost = aux_sln.cost
-                current_ite = 0
+                crnt_it = 0
                 break
 
         del aux_sln
 
+    return sln
+
+def annealing(sln):
+    t_max = math.floor(sln.n * ANNEALING_TEMPERATURE_FACTOR)
+    t = t_max
+    s_cost = sln.cost
+
+    while t > 0.0:
+        aux_sln = sln.copy()
+        aux_sln.randomize(int(aux_sln.n * (t / t_max)))
+
+        # search(aux_sln, 0.8)
+
+        diff_cost = sln.cost - aux_sln.cost
+
+        if diff_cost > 0 or \
+           math.exp(float(diff_cost) / t) > random.uniform(0,1):
+            sln.permutation = aux_sln.permutation
+            sln.flows = aux_sln.flows
+            sln.cost = aux_sln.cost
+
+        del aux_sln
+        t = math.floor(0.73 * t)
+
+    return sln
+
 def search(sln, iterations_coeff=LOCAL_SEARCH_COEFFICIENT):
-    options = xrange(0,sln.n)
-    a_random = random.SystemRandom()
-    
     for _ in xrange(0, int(iterations_coeff * sln.n)):
-        city_1 = a_random.choice(options)
-        city_2 = a_random.choice(options)
+        cities = randomOptions(sln.n, k=2)
+        aux_cost = sln.cost
 
-        aux_sol_cost = sln.cost
+        sln.exchangeFacilities(cities[0], cities[1])
 
-        sln.exchangeFacilities(city_1, city_2)
+        if aux_cost < sln.cost:
+            sln.exchangeFacilities(cities[0], cities[1])
 
-        if aux_sol_cost < sln.cost:
-            sln.exchangeFacilities(city_1, city_2)
+    return sln
